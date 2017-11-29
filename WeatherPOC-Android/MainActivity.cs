04ba740227d60f3fcc5aa0829567112f1b91931e
AlvaroTorrez <alvaro.torrez.salas@outlook.com>
2017-11-28 20:17:58 -0400

    Story #1: Create login view - Update a clean screen view to work on android - Update the manifest

--- WeatherPOC-Android/MainActivity.cs
+++ WeatherPOC-Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.Widget;
 using Android.OS;
 
 namespace WeatherPOC_Android {
-    [Activity(Label = "WeatherPOC_Android", MainLauncher = true)]
+    [Activity(Label = "WeatherPOC_Android", MainLauncher = true, Theme = "@android:style/Theme.DeviceDefault.NoActionBar.Fullscreen")]
     public class MainActivity : Activity {
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
