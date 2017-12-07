07039808739d91b920c72957c1180227cba934ff
AlvaroTorrez <alvaro.torrez.salas@outlook.com>
2017-12-07 19:13:12 -0400

    Story #1: Create login view - Added constants

--- WeatherPOC-ShareCode/GlobalConstants.cs
+++ WeatherPOC-ShareCode/GlobalConstants.cs
@@ -1,5 +1,6 @@
 ﻿namespace WeatherPOC_ShareCode {
     static public class GlobalConstants {
         public const string USER_SESSION = "UserSession";
+        public const string LOGIN_USER_INFORMATION = "LoginUserInformation";
     }
 }
